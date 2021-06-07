1350a107f8abeadfcfaa528b6e839a9a5c0727b5
kdomi18 <kdomi18@epoka.edu.al>
2021-06-07 22:04:37 +0200

    no backend so far just simple outline + authentication

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::get('/crop', function () {
+    return view('crop');
+})->middleware(['auth'])->name('crop');
+
+Route::get('/livestock', function () {
+    return view('livestock');
+})->middleware(['auth'])->name('livestock');
+
+Route::get('/market', function () {
+    return view('market');
+})->middleware(['auth'])->name('market');
+
+Route::get('/weather', function () {
+    return view('weather');
+})->middleware(['auth'])->name('weather');
+
+Route::get('/vet', function () {
+    return view('vet');
+})->middleware(['auth'])->name('vet');
+
+Route::get('/users', function () {
+    return view('users');
+})->middleware(['auth'])->name('users');
